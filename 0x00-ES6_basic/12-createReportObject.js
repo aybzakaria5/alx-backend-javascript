4855e2fc048d50e64ab51f94e3ff7caf89210b86
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-08 22:36:32 +0000

    task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+}
+
