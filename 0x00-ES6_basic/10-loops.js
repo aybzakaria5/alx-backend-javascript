ef42d6e7fa6350b875db87eed41f4a6283694a4c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-08 22:23:45 +0000

    using for of task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
+
