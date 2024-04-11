fc37745ec73a7c30b70713116b515f4831d91a7a
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-11 17:09:51 +0000

    fixing using eslint

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
-
