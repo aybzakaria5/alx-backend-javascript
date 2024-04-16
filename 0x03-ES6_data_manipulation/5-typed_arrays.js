57e3106abf2f32cff52c53bfa4484b6bdefd7a14
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-16 23:07:52 +0100

    task 5 done

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+function createInt8TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+      throw new Error('Position outside range');
+    }
+  
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer);
+    view.setInt8(position, value);
+  
+    return view;
+  }
+  
+  export default createInt8TypedArray;
+  
