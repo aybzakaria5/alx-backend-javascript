8bd0d01539b31aecb1932e8b961bd38ea5f3f4a1
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-16 22:55:12 +0100

    task 1 done

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+  
+    return students.map(student => student.id);
+  }
+  
+  export default getListStudentIds;
+  
