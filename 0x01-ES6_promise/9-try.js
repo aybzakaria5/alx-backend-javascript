7265469f2328494204b528b4ec3f3220aa805bc4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-02 15:42:15 +0100

    tasl 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(`Error: ${error.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }
+  
+  export default guardrail;
+  
